9af16a8566fb731932c78713b2bd3dc9c76c1016
andreidushev <58424742+andreidushev@users.noreply.github.com>
2020-11-23 14:40:03 +0200

    Add files via upload

---
+++ Applied Arithmetics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int, int> add = n => n + 1;
+            Func<int, int> multiply = n => n * 2;
+            Func<int, int> subtract = n => n - 1;
+            Action<int[]> print = n => Console.WriteLine(string.Join(" ",n));
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        nums = nums.Select(n => add(n)).ToArray();
+                        break;
+                    case "multiply":
+                        nums = nums.Select(n => multiply(n)).ToArray();
+                        break;
+                    case "subtract":
+                        nums = nums.Select(n => subtract(n)).ToArray();
+                        break;
+                    case "print":
+                        print(nums);
+                        break;
+
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
